e123abf2328471c5666d8de67b4265b26ce93d2f
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-01-06 21:41:26 -0300

    console.log removed

--- server/data/users-data.ts
+++ server/data/users-data.ts
@@ -13,7 +13,6 @@ export default class UserData {
     public getByEmailPasswordDomain = ({ email, domainkey, domain }: UserDataType) => db.query(`select * from user_profile where email = '${email}' and domainkey = '${domainkey}' and domain = '${domain}'`);
 
     public updateUser = ({ email, domainkey, domain, newemail, newpassword }: UserDataType) => {
-        console.log(email, domain, domainkey, newemail, newpassword);
         if (newemail && newpassword) return db.query(`UPDATE user_profile SET email = '${newemail}', password = '${newpassword}' WHERE email = '${email}' and domain = '${domain}' and domainkey = '${domainkey}'`);
         else if (newemail) return db.query(`UPDATE user_profile SET email = '${newemail}' WHERE email = '${email}' and domain = '${domain}' and domainkey = '${domainkey}'`);
         else if (newpassword) return db.query(`UPDATE user_profile SET password = '${newpassword}' WHERE email = '${email}' and domain = '${domain}' and domainkey = '${domainkey}'`);
